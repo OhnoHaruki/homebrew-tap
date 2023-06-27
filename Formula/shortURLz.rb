7eeb894eed3cd89ed9dc056a0df2c0f987d8bf20
OhnoHaruki <haruki20010219@gmail.com>
2023-06-27 12:17:50 +0900

    make shortURLz.rb

---
+++ Formula/shortURLz.rb
@@ -0,0 +1,17 @@
+VERISON="1.0.0"
+class shortURLz < Formula
+    desc "URL shortner via bit.ly, etc."
+    homepage "https://ohnoharuki.github.io/shortURLz"
+    url "https://github.com/OhnoHaruki/shortURLz/releases/download/v#{VERSION}/shortURLz-#{VERSION}_darwin_amd64.tar.gz"
+    version VERSION
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    license "MIT"
+    option "without-completion@2" => :optional
+    def install
+        bin.install"shortURLz"
+        bash_completion.install "completions/bash/shortURLz" if build.with? "completions"
+    end
+    test do
+        system bin/"shortURLz","--version"
+    end
+end
