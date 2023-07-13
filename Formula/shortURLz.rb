3813d6331313c95da89711b9c9bbeb1f77877e13
OhnoHaruki <haruki20010219@gmail.com>
2023-07-13 15:40:41 +0900

    to edit some folder

--- Formula/shortURLz.rb
+++ Formula/shortURLz.rb
@@ -3,7 +3,7 @@ VERSION="0.1.11"
 class Shorturlz < Formula
     desc "URL shortner via bit.ly, etc."
     homepage "https://ohnoharuki.github.io/shortURLz"
-    url "https://github.com/OhnoHaruki/shortURLz/releases/download/v#{VERSION}/shortURLz-#{VERSION}_darwin-amd64.tar.gz"
+    url "https://github.com/OhnoHaruki/shortURLz/releases/download/v#{VERSION}/shortURLz-#{VERSION}_darwin_amd64.tar.gz"
     version VERSION
     sha256 "34122730847a2dbd7683352efcab5981c730d6506fe4d0009e1048c20bb17e56"
     license "MIT"
