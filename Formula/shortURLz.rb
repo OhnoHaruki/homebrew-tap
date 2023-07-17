a84e8daa87678d71a0e6bbccd23bb67f0bad1b2a
OhnoHaruki <haruki20010219@gmail.com>
2023-07-17 15:24:24 +0900

    to edit dist and sha256

--- Formula/shortURLz.rb
+++ Formula/shortURLz.rb
@@ -5,7 +5,7 @@ class Shorturlz < Formula
     homepage "https://ohnoharuki.github.io/shortURLz"
     url "https://github.com/OhnoHaruki/shortURLz/releases/download/v#{VERSION}/shortURLz-#{VERSION}_darwin_amd64.tar.gz"
     version VERSION
-    sha256 "34122730847a2dbd7683352efcab5981c730d6506fe4d0009e1048c20bb17e56"
+    sha256 "a7fceb0efb234e1e780bef00fb551f107ba976e3cfa7e93f2e70e2de655657f3"
     license "MIT"
 #    option "without-completions", "Disable bash completions"
 #    depends_on "without-completion@2" => :optional
